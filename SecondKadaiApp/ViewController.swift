e9807636bb0fdcc28c5278f308052c47a4010faf
大森直人 <naoto.omori@mscn2002.local>
2016-06-29 19:46:27 +0900

    あいさつアプリを作成した。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yourName: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let resultViewController:ResultViewController = segue.destinationViewController as! ResultViewController
+        
+        resultViewController.yourName = self.yourName.text!
+    }
 
+    @IBAction func back(segue: UIStoryboardSegue){
+        
+    }
 }
 
